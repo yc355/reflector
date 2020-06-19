cd1b84b85baa84ae742d4d15a0ae62ffb64104a7
Yifan <yifan.i.chen@gmail.com>
2020-06-19 14:10:39 -0400

    replace translated iframe with embed

--- src/components/translated.jsx
+++ src/components/translated.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Iframe from 'react-iframe'
 import './translated.css'
 
-export default class Translated extends Component {
-  render() {
-    return (
-      <div class='iframes'>
-        <div class='iframe-container'>
-          <Iframe url="http://stoiccamel.ru/peter-the-great/" frameBorder='0'/>
-        </div>
-        <div class='iframe-container'>
-          <Iframe url="http://stoiccamel.ru/peter-the-great/" frameBorder='0'/>
-        </div>
+export default function Translated() {
+  return (
+    <div class='iframes'>
+      <div class='control'>
+        Control
       </div>
-    )
-  }
+      <div class='iframe-container'>
+        <Iframe url="http://stoiccamel.ru/peter-the-great/" frameBorder='0' id='translated'/>
+      </div>
+      <div class='iframe-container'>
+        <embed src="https://translate.google.com/translate?hl=&sl=ru&tl=en&u=http%3A%2F%2Fstoiccamel.ru%2Fpeter-the-great%2F"/>
+      </div>
+    </div>
+  )
 }
